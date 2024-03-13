33b5649e4e4bf35a2f593e82da1830424cc4f07e
lekhnath <lekhnath.yadav1991@gmail.com>
2024-03-13 09:31:24 +0545

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+var container = document.querySelector("#container");
+var loveicon = document.querySelector("i");
+
+container.addEventListener("dblclick",function(){
+    loveicon.style.transform = "translate(-50%,-50%) scale(1)";
+    loveicon.style.opacity =0.8;
+
+    setTimeout(function(){
+        loveicon.style.opacity=0
+    },1000);
+    setTimeout(function(){
+        loveicon.style.transform = "translate(-50%,-50%) scale(0)";
+    },2000);
+});
